38a37926154cda19986874501d8c683d6514bdcd
leshe4ka46 <alex9102naid1@ya.ru>
2024-04-14 15:28:12 +0300

    fix unknown user handling

--- handlers/info.go
+++ handlers/info.go
@@ -16,26 +16,30 @@ var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func RandStringRunes(n int) string {
 	rand.Seed(time.Now().UnixNano())
-    b := make([]rune, n)
-    for i := range b {
-        b[i] = letterRunes[rand.Intn(len(letterRunes))]
-    }
-    return string(b)
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func makeNewUser(database *gorm.DB,c *gin.Context) {
+	uid := base64.RawStdEncoding.EncodeToString(uuid.NewV4().Bytes())
+	c.SetCookie(consts.LoginCookieName, uid, consts.CookieLifeTime, "/", consts.CookieDomain, consts.SecureCookie, true)
+	db.AddUser(database, &db.Gocheck{Cookies: uid, Username: RandStringRunes(64)})
 }
 
 func InfoHandler(database *gorm.DB, userProvider *store.SessionProvider) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
 		cookie, err := c.Cookie(consts.LoginCookieName)
-		if err != nil || cookie=="" {
-			uid := base64.RawStdEncoding.EncodeToString(uuid.NewV4().Bytes())
-			c.SetCookie(consts.LoginCookieName, uid, consts.CookieLifeTime, "/", consts.CookieDomain, consts.SecureCookie, true)
-			userProvider.Set(&store.UserSession{Cookie: uid})
-			db.AddUser(database, &db.Gocheck{Cookies: uid, Username: RandStringRunes(64)})
+		if err != nil || cookie == "" {
+			makeNewUser(database,c)
 			c.JSON(404, gin.H{"error": "User not found"})
 			return
 		}
 		user, err := db.GetUserByCookie(database, cookie)
-		if err != nil || user.Password=="" {
+		if err != nil || user.Password == "" {
+			makeNewUser(database,c)
 			c.JSON(404, gin.H{"error": "User not found"})
 			return
 		}
